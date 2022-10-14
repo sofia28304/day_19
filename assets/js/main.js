aa623f2788b9bd54e5eebfd7e58a83bcb378f656
sofia28304 <sofia.vogelhaupt@yahoo.de>
2022-10-14 20:07:33 +0200

    design critique

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+
+// Design for mobile displays from 360×640 through 414×896
+// Design for tablet displays from 601×962 through 1280×800
+// Design for desktop displays from 1024×768 through 1920×1080
+// aus https://www.hobo-web.co.uk/best-screen-size/
